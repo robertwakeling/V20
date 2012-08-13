a8b6cbdfb86099804dc46c90a65da9be4e1ddad2
Robert Wakeling <robertwakeling82@gmail.com>
2012-08-13 15:06:05 +0100

    Third

---
+++ Mr Wakeling's Science Year 7/HabitatsAssessmentViewController.h
@@ -0,0 +1,13 @@
+//
+//  HabitatsAssessmentViewController.h
+//  Mr Wakeling's Science Year 7
+//
+//  Created by Robert Wakeling on 13/08/2012.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HabitatsAssessmentViewController : UIViewController
+
+@end
---
+++ Mr Wakeling's Science Year 7/HabitatsAssessmentViewController.m
@@ -0,0 +1,43 @@
+//
+//  HabitatsAssessmentViewController.m
+//  Mr Wakeling's Science Year 7
+//
+//  Created by Robert Wakeling on 13/08/2012.
+//
+//
+
+#import "HabitatsAssessmentViewController.h"
+
+@interface HabitatsAssessmentViewController ()
+
+@end
+
+@implementation HabitatsAssessmentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
